34b687caa2267152d2ec3ca8ac3affe5c88d9fcb
blackalbino01 <hauwal4969@gmail.com>
2023-04-25 09:50:10 +0100

    update

--- price-range-bot.js
+++ price-range-bot.js
@@ -6,23 +6,23 @@ const request = require('request');
 module.exports = async function runBot() {
     setInterval(async () => {
         try {
-          // Retrieve the token price
-          /*const tokenPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=your_token_name&vs_currencies=usd';
-          const tokenPrice = await request(tokenPriceURL);
-          const tokenPriceUSD = JSON.parse(tokenPrice).your_token_name.usd;
+          //Retrieve the token price
+          const tokenPriceURL = await fetch('https://api.binance.com/api/v1/ticker/price?symbol=BNBUSDT');
+          const tokenPrice = await tokenPriceURL.json();
+          const tokenPriceUSD = tokenPrice.price;
       
           // Calculate the optimal LP range
-          const lowerPriceBound = tokenPriceUSD * 0.9;
-          const upperPriceBound = tokenPriceUSD * 1.1;
+          const lowerPriceBound = tokenPriceUSD - 1;
+          const upperPriceBound = tokenPriceUSD + 1;
       
           // Set the LP range using the PancakeSwap V3 API
-          await pancakeSwapV3Contract.methods.setTickSpacing(60).send({ from: your_wallet_address, gasPrice: your_gas_price });
-          await pancakeSwapV3Contract.methods.initialize(your_token0_address, your_token1_address, lowerPriceBound, upperPriceBound, 0, 0).send({ from: your_wallet_address, gasPrice: your_gas_price });*/
-            console.log("first")
+          await pancakeSwapV3Contract.methods.setTickSpacing(60).send({ from: process.env.WALLET_ADDRESS });
+          await pancakeSwapV3Contract.methods.initialize(process.env.BNB, process.env.BUSD, lowerPriceBound, upperPriceBound, 0, 0).send({ from: process.env.WALLET_ADDRESS });
+            console.log(tokenPriceUSD);
         } catch (err) {
           console.error(`Error updating LP range: ${err}`);
         }
-      }, 1000); // Execute the loop every minute (60 seconds)
+      }, 1000); // Execute the loop every second (60 seconds)
       
 };
   
